627874d77b37bb609b21638d7d73a197f34e45a3
Quách Vinh Quang <107240917+hynoras@users.noreply.github.com>
2024-05-20 15:59:13 +0700

    Update deviceModel.js

--- backend/model/deviceModel.js
+++ backend/model/deviceModel.js
@@ -1,15 +1,25 @@
-const mongoose = require("mongoose");
-const { Schema, model } = mongoose;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const DeviceSchema = new Schema(
-  {
-    device_name: String,
-    sensor_type: String,
-    // isActive: Boolean,
-    lastActive: { type: Date, default: Date.now },
+const sensorDataSchema = new mongoose.Schema({
+  deviceid: {
+    type: Schema.Types.ObjectId,
+    ref: 'Device'
+  },
+  sensorid: {
+    type: Schema.Types.ObjectId,
+    ref: 'SensorType'
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now
+  },
+  value: {
+    type: Number,
+    required: true
   }
-);
+});
 
-const DeviceModel = model("Device", DeviceSchema);
+const SensorData = mongoose.model('SensorData', sensorDataSchema);
 
-module.exports = DeviceModel;
+module.exports = SensorData;
